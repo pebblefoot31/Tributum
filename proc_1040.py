acf07de9f1da056eb5c19d7450bdc28e013eacc5
Nia Maheshwari <unikorn.nkm31@gmail.com>
2020-12-31 18:21:10 -0500

    Created and tested a code to calculate the num of dependents.

--- proc_1040.py
+++ proc_1040.py
@@ -2,7 +2,36 @@
 
 import toml
 
-d_1040 = toml.load("f1040.toml")
+def deps(d):
+    count = 0
+    #while
+    x = 0
+    while x < 4:
+        x += 1
+        if d["Dep" + str(x)]["FN_LN"] != "":
+            count += 1
+    
+    return(count)
 
-print(d_1040)
+def start():
+    d_1040 = toml.load("f1040.toml")
+
+    #print(d_1040)
+
+    #print(d_1040["Dependents"]["Dep1"])
+
+    print(d_1040["Dep1"]["FN_LN"])
+
+    print(d_1040["Address"]["Street"])
+
+    print(deps(d_1040))
+
+
+
+if __name__ == "__main__":
+
+    start()
+
+
+    
 
